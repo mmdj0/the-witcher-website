e5d0525e3d0b763d003ea591a153f172d2c61710
maroua <95887788+mmdj0@users.noreply.github.com>
2022-08-31 02:44:32 +0100

    Add files via upload

---
+++ forget password page/forgetpsw.php
@@ -0,0 +1,61 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="forgetpsw-style.css" type="text/css">
+
+    <!-- Font Awesome Icons  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+
+    <!-- Google Fonts  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <title>Forgot Password </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yuji+Mai&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="root">
+        <header>
+            <nav>
+            <div id="global-logo-div"><a href="#home-container"><img src="witcher-logo.png"></a><div id="title-logo-div"><div id="typed-out"><h4>The Witcher.</h4></div></div></div>                <div>
+                    <ul>
+                        <li>
+                            <a href="index.html#about-section">About</a>
+                        </li>
+                        <li>
+                            <a href="index.html#services-section">Services</a>
+                        </li>
+                        <li>
+                            <a href="index.html#prices-section">Prices</a>
+                        </li>
+                        <li id="signin-container">
+                            <a href="signin.php" id="signin-link">Sign in</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div id="eclips1"></div>
+        <div id="eclips2"></div>
+        <div class="card">
+            <p><img id="lock-icon" src="lockIcon-removebg.png" ></p>
+            <h2>Forgot Password?</h2>
+            <p>You can reset your Password here</p>
+            <input type="email" class="passInput" placeholder="Email address">
+            <button>Send My Password</button>
+        </div>
+    </div>
+    
+</body>
+
+</html>
